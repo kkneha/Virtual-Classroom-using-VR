72cefdd6f99d189ea0b0304891d28e67a0125f20
Shraddha Chobhe <47939285+shraddhaz25@users.noreply.github.com>
2020-12-07 19:32:22 +0530

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import os
+
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("say something...")
+        r.adjust_for_ambient_noise(source)
+        audio_data = r.listen(source)
+        try:
+            text = r.recognize_google(audio_data)
+            if text == "exit":
+                break
+            else:
+                print(text)
+        except:
+            print('Internet connectivity issue')
